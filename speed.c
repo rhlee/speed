75fda94f148ec389805f44cdde6a8e09d707c41f
richard <lenovo@lenovo.(none)>
2012-04-20 23:28:08 +0100

    file access

--- speed.c
+++ speed.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(int argc, char *argv[])
 {
+  FILE *inputFile, *outputFile;
+  long inputSample, outputSample;
+
+  inputFile = fopen("sample.wav", "r");
+  outputFile = fopen("test.wav", "w");
+  if(setvbuf(inputFile, NULL, _IOFBF, 4096) ||
+    setvbuf(outputFile, NULL, _IOFBF, 4096))
+  {
+    perror("Could not set file buffer\n");
+    exit(1);
+  }
+
+  for(; 0;)
+  {
+    
+  }
+
+  fclose(inputFile);
+  fclose(outputFile);
   return 0;
 }
