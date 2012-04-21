bc351b67cf66a051dd9d07cd470b28a5565e030f
richard <lenovo@lenovo.(none)>
2012-04-21 15:52:00 +0100

    Time

--- speed.c
+++ speed.c
@@ -4,8 +4,9 @@
 
 int main(int argc, char *argv[])
 {
+  double factor = (double)1001 / (double)960;
+  
   FILE *inputFile, *outputFile;
-  long inputSample, outputSample;
 
   inputFile = fopen("sample.wav", "r");
   outputFile = fopen("test.wav", "w");
@@ -16,9 +17,19 @@ int main(int argc, char *argv[])
     exit(1);
   }
 
-  for(; 0;)
+  if(sizeof(short) != 2)
   {
-    
+    perror("Short is not 2 bytes\n");
+    exit(1);
+  }
+
+  long inputSample = -1, outputSample = 0, inputSampleFloor;
+  double inputTime;
+  factor = 2;
+  for(outputSample = 0; outputSample < 10; outputSample++)
+  {
+    inputTime = factor * ((long)outputSample + 0.5);
+    printf("%f\n", inputTime);
   }
 
   fclose(inputFile);
